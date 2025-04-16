704b58d0294b4dffcbd6517e558a3f789baacf70
EduardoShande <eduardoshande123@gmail.com>
2025-04-16 14:16:35 -0400

    I connected postgreSQL to all microservices

--- microservices/user_service/src/dj_user_service/settings.py
+++ microservices/user_service/src/dj_user_service/settings.py
@@ -73,11 +73,10 @@ WSGI_APPLICATION = 'dj_user_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
 
 
