c6a5946f8b3388e16b81590145db380ec1bd872d
EduardoShande <eduardoshande123@gmail.com>
2025-04-28 17:22:26 -0400

    New updates

--- microservices/hotel_service/src/apps/hotel/models.py
+++ microservices/hotel_service/src/apps/hotel/models.py
@@ -34,7 +34,7 @@ class Service(models.Model):
 
 class Room(models.Model):
     hotel = models.ForeignKey('Hotel', on_delete=models.CASCADE, related_name='rooms')
-    type = models.ForeignKey('RoomType', on_delete=models.PROTECT, related_name='rooms')
+    room_type = models.ForeignKey('RoomType', on_delete=models.PROTECT, related_name='rooms')
     number = models.CharField(max_length=10, unique=True)  # Room number, unique per room
     image = models.ImageField(upload_to='room_images/', blank=True, null=True)
     price_per_night = models.DecimalField(max_digits=8, decimal_places=2)  # Example: 150.00
--- microservices/hotel_service/src/dj_hotel_service/urls.py
+++ microservices/hotel_service/src/dj_hotel_service/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings  # Import settings here
 from django.conf.urls.static import static  # Import static helper function
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hotel/', include('apps.hotel.urls'))
 ]
 
 if settings.DEBUG:
